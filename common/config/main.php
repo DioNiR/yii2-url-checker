e768623c934497e2cdffc2307930b4f7a335f01d
dionir90 <dionir90@gmail.com>
2024-11-29 22:53:51 +0300

    setting_rabbitmq

--- common/config/main.php
+++ common/config/main.php
@@ -4,29 +4,23 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'vendorPath' => dirname(__DIR__, 2) . '/vendor',
     'components' => [
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
 
         'db' => [
-            'class' => \yii\db\Connection::class,
-            'dsn' => sprintf('mysql:host=%s;dbname=%s', getenv('MYSQL_HOST'), getenv('MYSQL_DATABASE')),
+            'class'    => \yii\db\Connection::class,
+            'dsn'      => sprintf('mysql:host=%s;dbname=%s', getenv('MYSQL_HOST'), getenv('MYSQL_DATABASE')),
             'username' => getenv('MYSQL_USER'),
             'password' => getenv('MYSQL_USER'),
-            'charset' => 'utf8',
+            'charset'  => 'utf8',
         ],
 
         'queue' => [
-            'queue' => [
-                'class' => \yii\queue\amqp\Queue::class,
-                'host' => 'localhost',
-                'port' => 5672,
-                'user' => 'guest',
-                'password' => 'guest',
-                'queueName' => 'queue',
-            ],
+            'class' => \yii\queue\amqp_interop\Queue::class,
+            'dsn' => sprintf('amqp://%s:%s@%s:%s/%%2F', getenv('RABBITMQ_USER'), getenv('RABBITMQ_PASSWORD'), getenv('RABBITMQ_HOST'), getenv('RABBITMQ_PORT')),
         ],
     ],
 ];
