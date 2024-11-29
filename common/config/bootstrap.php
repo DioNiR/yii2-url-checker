35fc55f94c4164324fb8669103617aa004223b3e
dionir90 <dionir90@gmail.com>
2024-11-29 22:05:12 +0300

    init_yii

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,4 +1,23 @@
 <?php
+$env = Dotenv\Dotenv::createUnsafeImmutable(dirname(dirname(__DIR__)));
+$env->load();
+
+$env->required('APP_FRONTEND_HOST');
+$env->required('APP_FRONTEND_URL');
+$env->required('APP_FRONTEND_COOKIE_SALT');
+$env->required('APP_BACKEND_HOST');
+$env->required('APP_BACKEND_URL');
+$env->required('APP_BACKEND_COOKIE_SALT');
+
+$env->required('YII_ENV')->allowedValues(['prod', 'dev']);
+$env->required('YII_DEBUG')->isBoolean();
+$env->required('YII_TRACE_LEVEL')->isInteger();
+
+$env->required('MYSQL_HOST');
+$env->required('MYSQL_USER');
+$env->required('MYSQL_PASSWORD');
+$env->required('MYSQL_DATABASE');
+
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
